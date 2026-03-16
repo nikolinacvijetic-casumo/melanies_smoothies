73c9d8df17a560e33bc85bebd28048e0df7d272e
nikolinacvijetic-casumo <nikolina.cvijetic@casumo.com>
2026-03-16 16:21:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,5 +64,5 @@ if time_to_insert:
 
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
